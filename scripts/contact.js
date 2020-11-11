9096f8ad4db6d1cee7adf340886dcc426e786446
RoseGoldHeart <74288871+RoseGoldHeart@users.noreply.github.com>
2020-11-11 08:27:52 +0100

    Website made

---
+++ scripts/contact.js
@@ -0,0 +1,42 @@
+const header = document.getElementById('header');
+const backgroundHeader = document.getElementById('backgroundHeader');
+const home = document.getElementById('home');
+const photos = document.getElementById('photos');
+const poems = document.getElementById('poems');
+const film = document.getElementById('film');
+const articles = document.getElementsByTagName('ARTICLE');
+const profile = document.getElementById('profile');
+const line = document.getElementById('line');
+const about = document.getElementById('about');
+const cross = document.getElementById('cross');
+const click = document.getElementById('click');
+
+function clickHeader() {
+	header.style.width = '110%';
+	header.style.height = '50%';
+	profile.style.marginTop = '5%';
+	line.style.marginTop = '7.5%';
+	about.style.marginTop = '6%';
+	cross.style.marginTop = '-0.2%';
+
+};
+function stopClickHeader() {
+	header.style.width = '4.5%';
+	header.style.height = '6.6%';
+	profile.style.marginTop = '-50%';
+	line.style.marginTop = '-10%';
+	about.style.marginTop = '-10%';
+	cross.style.marginTop = '-5%';
+};
+
+click.addEventListener('click', clickHeader);
+cross.addEventListener('click', stopClickHeader);
+
+
+
+
+function hoverArticle()  {
+};
+
+articles.addEventListener('mouseover', hoverArticle);
+
